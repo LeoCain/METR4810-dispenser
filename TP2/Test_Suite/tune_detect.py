88ea2a0c911923abf1a92fd3d7312e14d3aaf1e3
xspinella <xavierspinella@gmail.com>
2022-04-07 23:11:07 +1000

    tune_detect.py function updated

--- TP2/Test_Suite/tune_detect.py
+++ TP2/Test_Suite/tune_detect.py
@@ -2,4 +2,31 @@ import pigpio as pp
 from matplotlib import pyplot as plt
 import numpy as np
 
+# define pin to be tested:
+test_pin = 24   # 24 for IR1, 25 for HAND
+# Initialise pigpio object and read pin
 pi = pp.pi()
+pi.set_mode(test_pin, pp.INPUT)
+
+# read from pin
+pi.read(test_pin)
+
+# plot input real time
+x=-0.04
+while True:
+    x=x+0.04
+    y = pi.read(test_pin)
+    plt.scatter(x, y)
+    plt.title("Real Time plot")
+    plt.xlabel("x")
+    plt.ylabel("pin input")
+    plt.xlim(50)
+    if pi.read(test_pin) == 1 :
+        pi.set_pull_up_down(test_pin, pp.PUD_DOWN)
+    else :
+        pi.set_pull_up_down(test_pin, pp.PUD_UP)
+    plt.pause(0.01)
+
+# plt.show()
+
+
