498445e3a960b80daa09fe44f5a914ab75b3cece
xspinella <xavierspinella@gmail.com>
2022-05-30 05:35:25 +1000

    small chamges

--- TP2/Test_Suite/LEDtest.c
+++ TP2/Test_Suite/LEDtest.c
@@ -13,17 +13,10 @@ int main(void) {
     setup();
     signal(SIGINT, safe_terminate);
     int i = 0;
-<<<<<<< HEAD
-    while (i < 100) {
-        gpioWrite(IR1, 0); // turn on LED
-        sleep(1); // wait 1 second
-        gpioWrite(IR1, 0);
-=======
     while (running2) {
         gpioWrite(LEDs, 1); // turn on LED
         sleep(1); // wait 1 second
         gpioWrite(LEDs, 0);
->>>>>>> 0cb0f80de062f081e34372a8af1856c8f8463a3c
         sleep(1); // wait 1 second
         i++;
     }
